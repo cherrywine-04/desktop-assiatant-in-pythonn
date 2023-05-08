5b8d2410f4d92fd4a9b295be9fe5b74215194032
PRIYA <76675024+cherrywine-04@users.noreply.github.com>
2023-05-08 23:19:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,115 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+import pyjokes
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishme():
+    hour = int(datetime.datetime.now().hour)
+    if hour>=0 and hour<12:
+        speak("Good Morning Sunshine, Rise and Shine !")
+
+    elif hour>=12 and hour<18:
+        speak("Good Afternoon!")   
+
+    else:
+        speak("Good Evening!")  
+
+    speak("I am Takao. Please tell me how may I help you")       
+
+def takeCommand():
+    #It takes microphone input from the user and returns string output
+
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")    
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        # print(e)    
+        print("Say that again please...")  
+        return "None"
+    return query
+
+
+
+if __name__ == "__main__":
+    wishme()
+    while True:
+    # if 1:
+        query = takeCommand().lower()
+
+        # Logic for executing tasks based on query
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            print(results)
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")   
+        elif 'open codechef' in query:
+            webbrowser.open("codechef.com")
+        elif 'open hacker rank' in query:
+            webbrowser.open("hackerrank.com")
+        elif 'github' in query:
+            webbrowser.open("github.com")
+
+
+        elif 'play music' in query:
+            music_dir = "C:\\Users\\PS\\Music"
+            songs = os.listdir(music_dir)
+            print(songs)    
+            os.startfile(os.path.join(music_dir, songs[0]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")    
+            speak(f"Sir, the time is {strTime}")
+
+        
+
+        elif "what's your name" in query or "What is your name" in query:
+            speak("My name is TAKAO.")
+            print("My name is  TAKAO")
+        elif 'exit' in query:
+            speak("Thanks for giving me your time")
+            exit()
+        elif "who made you" in query or "who created you" in query:
+            speak("I have been created by Priya.")
+            print("I have been created by Priya.")
+        elif 'joke' in query:
+            speak(pyjokes.get_joke())
+        elif "taka" in query:
+            speak("Yes, i am listening, Priya")
+        
+
+        
 
